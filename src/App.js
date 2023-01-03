3edf9a3bdd608579271ca3eb0c9a68f80fd3b403
Salman Shaikh <sk.salman0425@gmail.com>
2023-01-03 12:11:21 +0530

    changes done in routing

--- src/App.js
+++ src/App.js
@@ -1,48 +1,91 @@
-import './App.css';
-import React from 'react';
-import {BrowserRouter as Router,  Routes, Route, Link } from 'react-router-dom';
-//import {MemoryRouter as Router,  Routes, Route, Link } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+  useLocation
+} from "react-router-dom";
 
-// when we use MemoryRouter as Router, then it keeps the URL changes in memory instead of browser.
-//hence browser's forward and backward button will not work when we use MemoryBrowser as Router
-//MemoryRouter as Router is usefull for testing and non-browser enviroment like React native.
-function App() {
+export default function App() {
   return (
     <Router>
-      <div className='app'>
+      <div>
         <ul>
           <li>
-            <Link to={"/"}>Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to={"/about"}>About</Link>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to={"/users"}>Users</Link>
+            <Link to="/topics">Topics</Link>
           </li>
         </ul>
 
+        <Routes>
+          <Route path="/about" element={<About />}>
+            
+          </Route>
+          <Route path="/topics"element={ <Topics />}>
+           
+          </Route>
+          <Route path="/" element={  <Home />}>
+          
+          </Route>
+        </Routes>
       </div>
-
-    <Routes>
-      <Route exact path="/"element={<Home/>}></Route>
-      <Route exact path="about" element={<About/>}></Route>
-      <Route exact path="/users" element={<Users/>}></Route>
-    </Routes>
-
     </Router>
-    
   );
 }
-//these functions can be on seperate .js page
-function Home(){
-return <h2>Home Page</h2>
-}
-function About(){
-  return <h2>About Page</h2>
-}
-function Users(){
-  return <h2>Users Page</h2>
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
-export default App;
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  let match = useLocation();
+  console.log("Match::",match);
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Routes>
+        <Route path={`${match.path}/:topicId`} element={ <Topic />}>
+         
+        </Route>
+        <Route path={match.path} element={<><h3>Please select a topic.</h3></>}>
+          
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
+
+
